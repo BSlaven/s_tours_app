521c3f7fcbb677ebcd74a1792d584884fd81f814
Slaven B <slaven.bunijevac@gmail.com>
2022-01-29 21:29:28 +0100

    style card image inside a div

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -3,7 +3,18 @@ import classes from './Tour.module.css';
 const Tour = () => {
   return (
     <div className={classes.card}>
-      Ovo je tour komponenta
+      <div className={classes.imgContainer}>
+        <img
+          className={classes.image}
+          src="src/assets/building.jpg"
+          alt="a corporate building" />
+      </div>
+      <div className={classes.cardBody}>
+        This is card body
+      </div>
+      <div className={classes.cardFooter}>
+        This is a card footer
+      </div>
     </div>
   );
 };
