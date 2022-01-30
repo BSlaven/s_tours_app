3e968f76005d95cb0451be9dc5bcbd576759aafd
Slaven B <slaven.bunijevac@gmail.com>
2022-01-30 20:02:12 +0100

    introduce useState anad useEffect

--- src/components/Tours/Tours.jsx
+++ src/components/Tours/Tours.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect } from 'react'
+
 import Tour from '../Tour/Tour';
+
+const [ tours, setTours ] = useState({})
+
+useEffect(() => {
+  
+}, [])
+
+const fetchTours = async () => {
+  const res = await fetch('');
+  const tours = await res.jsosn();
+  setTours(tours);
+}
 
 const Tours = () => {
   return (
