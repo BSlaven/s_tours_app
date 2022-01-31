d9c7292e256a7fa38ac027cdc5024db6171188d6
Slaven B <slaven.bunijevac@gmail.com>
2022-01-31 13:12:29 +0100

    finish tours list styling

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -8,6 +8,10 @@ const Tour = ({ tour }) => {
         src={tour.image}
         alt={tour.name} />
       <div className={classes.cardBody}>
+        <div className={classes.cardHeader}>
+          <p className={classes.tourName}>{tour.name}</p>
+          <span className={classes.tourPrice}>{tour.price}</span>
+        </div>
         {tour.info.slice(0, 150)}
         <button className={classes.deleteBtn}>not interested</button>
       </div>
