79c16c8ba02c67c8c8ef5fbec66325be6722892f
Slaven B <slaven.bunijevac@gmail.com>
2022-02-01 12:32:06 +0100

    add remove functionality

---
+++ src/components/DeleteBtn/DeleteBtn.jsx
@@ -0,0 +1,11 @@
+import classes from './DeleteBtn.module.css';
+
+const DeleteBtn = ({ click, tourId}) => {
+  return (
+    <button onClick={() => click(tourId)} className={classes.deleteBtn}>
+      Not interested
+    </button>
+  )
+};
+
+export default DeleteBtn;
